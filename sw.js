31d86c5eb60b01fb6df88cecb719bee3eeb0950a
arkhan000648 <arkhan000648@gmail.com>
2025-10-20 21:13:47 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,10 +5,12 @@ self.addEventListener('fetch', (event) => {
 
 });
 
-
+// Monetag Verification.
 self.options = {
     "domain": "3nbf4.com",
     "zoneId": 10068797
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+// Monetag Verification END.
