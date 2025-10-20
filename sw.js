40659ce67797b05f1748148ca29abaee23c45fa8
arkhan000648 <arkhan000648@gmail.com>
2025-10-20 21:13:00 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,4 +2,13 @@
 self.addEventListener('fetch', (event) => {
   // This fetch handler is required to make the app installable.
   // It doesn't need to do anything special for a basic setup.
+
 });
+
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10068797
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
